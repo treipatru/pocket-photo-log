2f0c2bef183ff73aaf30576849e3d75bf14dad33
treipatru <andrei@planet34.org>
2024-07-22 10:36:10 +0200

    feat: add support for default image display

--- src/components/ui/file-input.tsx
+++ src/components/ui/file-input.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import FormError from "@/components/ui/form-error";
+import clsx from "clsx";
 
 interface Props {
 	classes?: string;
+	error?: string;
 	label: string;
 	name: string;
+	onChange: (file: InstanceType<typeof File>) => void;
+	defaultImgUrl?: string;
 	required?: boolean;
 	value: InstanceType<typeof File>;
-	error?: string;
-	onChange: (file: InstanceType<typeof File>) => void;
 }
 
 export default function FileInput(props: Props) {
-	const { value, label, error, onChange, ...inputProps } = props;
-	const [imagePreview, setImagePreview] = useState<string | null>(null)
+	const { value, label, error, onChange, defaultImgUrl, ...inputProps } = props;
+	const [imagePreview, setImagePreview] = useState<string | null>(defaultImgUrl || null)
 
 	const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const file = event.target?.files?.[0];
@@ -26,8 +28,14 @@ export default function FileInput(props: Props) {
 			};
 			reader.readAsDataURL(file);
 		} else {
-			setImagePreview(null);
+			setImagePreview(defaultImgUrl || null);
 		}
+	};
+
+	const handleImageReset = (event: React.SyntheticEvent) => {
+		event.preventDefault();
+		onChange(new File([], ''));
+		setImagePreview(defaultImgUrl || null);
 	};
 
 	return (
@@ -37,24 +45,43 @@ export default function FileInput(props: Props) {
 					<span className="label-text">{label}</span>
 				</div>
 
-				<input
-					{...inputProps}
-					accept="image/*"
-					className="file-input grow file-input-bordered w-full max-w-xs"
-					onChange={handleImageChange}
-					type="file"
-				/>
-			</label>
-
-			<div className="bg-muted w-full min-h-60">
-				{imagePreview && (
-					<img
-						id="imagePreview"
-						src={imagePreview}
-						alt="Image Preview"
+				<div className='flex items-center justify-between'>
+					<input
+						{...inputProps}
+						accept="image/*"
+						className="file-input file-input-bordered max-w-xs inline-block"
+						onChange={handleImageChange}
+						type="file"
 					/>
-				)}
-			</div>
+
+					{!!value.name && (
+						<button className="btn btn-square btn-outline" onClick={handleImageReset}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor">
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M6 18L18 6M6 6l12 12" />
+							</svg>
+						</button>
+					)}
+				</div>
+
+				<div className={clsx("w-full min-h-60 mt-4", { "bg-muted": !imagePreview })}>
+					{imagePreview && (
+						<img
+							id="imagePreview"
+							src={imagePreview}
+							alt="Image Preview"
+						/>
+					)}
+				</div>
+			</label>
 
 			{error && <FormError error={error} name={inputProps.name} />}
 		</>
