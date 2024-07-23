03ab4742c6437e978188de08cdee9c147e94be85
treipatru <andrei@planet34.org>
2024-07-23 17:22:00 +0200

    refactor: add regex to tag name validator

--- src/entities/tags.ts
+++ src/entities/tags.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const tagSchema = z.object({
 	id: z.string(),
-	name: z.string(),
+	name: z.string().regex(/^[a-zA-Z0-9\- ]+$/),
 });
 
 export type Tag = z.infer<typeof tagSchema>;
