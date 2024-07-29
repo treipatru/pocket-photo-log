290f4e45c0a3ce60b024af181443ca5ac23715c1
treipatru <andrei@planet34.org>
2024-07-29 17:43:06 +0200

    fix: hardcode api address
    Until a proper solution is found.

--- src/lib/get-img-url.ts
+++ src/lib/get-img-url.ts
@@ -1,11 +1,13 @@
 export type ImageSize = "650x650f" | "900x900f";
 
 /**
- * TODO: This can't be process.env as it is not available in the browser.
- * Verify that the value is not baked in the docker image at build time.
+ * TODO: This can't be in process.env but it should!
+ * Since this method is used in the browser too we need to make it
+ * available there too.
+ *
  */
 
-const apiUrl = import.meta.env.PUBLIC_API_URL;
+const apiUrl = "https://p34rev.planet34.org";
 
 export const getImgUrl = (
 	recordId: string,
