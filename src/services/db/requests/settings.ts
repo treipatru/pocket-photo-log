3f65aa8a44a94b0ce6c506c9bcb5b4029ed31e1d
treipatru <andrei@planet34.org>
2024-09-06 16:07:04 +0200

    feat: add updateSetting db request

--- src/services/db/requests/settings.ts
+++ src/services/db/requests/settings.ts
@@ -1,11 +1,32 @@
 import { dbClient } from "@/services/db/db";
 import { settingsArrayToObject } from "@/entities/settings";
+import type { Setting } from "@prisma/client";
 
 /**
- * Retrieve all settings
+ * Retrieve a list of settings from the database.
+ *
+ * @export
+ * @return {*}
  */
-export async function getSettings() {
+export async function getSettings(): Promise<Record<string, string>> {
 	const dbSettings = await dbClient.setting.findMany();
 
 	return settingsArrayToObject(dbSettings);
+}
+
+export async function updateSetting({
+	name,
+	value,
+}: {
+	name: string;
+	value: string;
+}): Promise<Setting> {
+	return await dbClient.setting.update({
+		where: {
+			name,
+		},
+		data: {
+			value,
+		},
+	});
 }
